8392cf12dd83b7e2f5f4138b6ebfb3f74a238810
Karl Merkli <k.merkli@eslgaming.com>
2017-08-17 13:57:50 +0200

    Added asciinema

--- install.sh
+++ install.sh
@@ -7,34 +7,40 @@ sudo apt-get update
 sudo apt-get install wpasupplicant=2.1-0ubuntu7
 sudo apt-mark hold wpasupplicant
 
-
-# Install vim
-sudo apt-get install vim
-
-# Install git
-sudo apt-get install git
-
 # Install chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
 
-# Install Curl
-sudo apt install curl
-
-# Install ZSH / oh-my-zsh
-sudo apt install zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-chsh -s $(which zsh)
-sudo printf "\nexec zsh" >> ~/.bash_profile
-sudo printf "\nexec zsh" >> ~/.bashrc
 
 # Franz
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2FAB19E7CCB7F415
 sudo sh -c 'echo "deb http://styrion.at/apt/ ./" >> /etc/apt/sources.list.d/styrion.list'
 sudo apt-get update
 sudo apt install meetfranz
+
+
+# CLI
+
+## Install vim
+sudo apt-get install vim
+
+## Install git
+sudo apt-get install git
+
+## Install Curl
+sudo apt install curl
+
+## Install ZSH / oh-my-zsh
+sudo apt install zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+chsh -s $(which zsh)
+sudo printf "\nexec zsh" >> ~/.bash_profile
+sudo printf "\nexec zsh" >> ~/.bashrc
+
+## Asciinema
+sudo apt-get install asciinema
 
 
 # Development
