31d1212939ea2e96a37d9d04bfa83e856662905a
Karl Merkli <k.merkli@eslgaming.com>
2017-08-17 13:53:23 +0200

    Added vscode

--- install.sh
+++ install.sh
@@ -57,3 +57,19 @@ sudo apt-get install -y build-essential
 
 ## Yarn
 sudo npm install -g yarn
+
+## IDEs
+
+### VS Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
+
+
+
+
+
+
